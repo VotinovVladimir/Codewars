56ed19afab3ab2c60a203dec9f6b2708bf364f03
VotinovVladimir <CodingGeek@yandex.ru>
2024-10-25 13:05:56 +0300

    Task1

---
+++ Upper.go
@@ -0,0 +1,7 @@
+package main
+
+import "strings"
+
+func Upper(s string) string {
+	return strings.ToUpper(s)
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	str := "ASSDFDFFGHF"
+	fmt.Println(Upper(str) == str)
+}
